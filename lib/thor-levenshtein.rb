73e4f8438f6705f5323deb6b143b4cb9e299f38c
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2012-12-31 22:06:42 -0500

    first commit

---
+++ lib/thor-levenshtein.rb
@@ -0,0 +1,7 @@
+require "thor-levenshtein/version"
+
+module Thor
+  module Levenshtein
+    # Your code goes here...
+  end
+end
---
+++ lib/thor-levenshtein/version.rb
@@ -0,0 +1,5 @@
+module Thor
+  module Levenshtein
+    VERSION = "0.0.1"
+  end
+end
